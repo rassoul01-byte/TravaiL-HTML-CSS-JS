c63f1b8fc859df2a7512995b604f49b08403dfa1
Seydina Wade <rassoulw01@gmail.com>
2026-04-27 20:07:01 +0000

    Correction bloc 1 css

--- BLOC1/exercice1.js
+++ BLOC1/exercice1.js
@@ -1,11 +1,10 @@
-//function changerTexte() {
-//        document.getElementById("monTitre").textContent = "promotion 8 dev data !";
- //       document.getElementById("monTitre").style.color = "red";
- //     }
+function changerTexte() {
+  // Changer le texte du titre
+  document.getElementById("monTitre").textContent = "Promotion 8 Dev Data !";
 
-let seydina = document.getElementById("bouton")
+  // Changer la couleur du texte
+  document.getElementById("monTitre").style.color = "red";
 
-seydina.addEventListener("click", function(){
-    document.getElementById("monTitre").textContent = "promotion 8 dev data !"
-    document.getElementById("monTitre").style.color = "red"
-})
+  // Changer le texte du bouton
+  document.getElementById("monBouton").textContent = "Cliqué !";
+}
